e234569af5bdec9d4bbce142cf8f790c6f5c1c6a
Nikhil Thakur <nikhilthakur8012004@gmail.com>
2025-05-21 03:18:26 +0530

    Add @vercel/analytics for improved analytics tracking

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,5 +1,10 @@
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import { Home } from "./components/Home/Home.jsx";
-
-createRoot(document.getElementById("root")).render(<Home />);
+import { Analytics } from "@vercel/analytics/react";
+createRoot(document.getElementById("root")).render(
+    <>
+        <Home />
+        <Analytics />
+    </>
+);
