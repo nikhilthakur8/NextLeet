fdda051247fbb68cafe9ca613b435a7c7d5a0fba
Nikhil Thakur <nikhilthakur8012004@gmail.com>
2025-05-25 09:11:19 +0530

    fix: Comment out Home component and update downtime message

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,7 +5,10 @@ import { Analytics } from "@vercel/analytics/react";
 import { SpeedInsights } from "@vercel/speed-insights/react";
 createRoot(document.getElementById("root")).render(
     <>
-        <Home />
+        {/* <Home /> */}
+        <p>
+            Hello World! This Website is temporarily down and will be back soon. 💀💀
+        </p>
         <Analytics />
         <SpeedInsights />
     </>
