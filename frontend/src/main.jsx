c741dc265fb159cc1e095576fb55350349e225a3
Nikhil Thakur <nikhilthakur8012004@gmail.com>
2025-05-25 07:12:42 +0530

    change

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,7 +2,7 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import { Home } from "./components/Home/Home.jsx";
 import { Analytics } from "@vercel/analytics/react";
-import { SpeedInsights } from "@vercel/speed-insights/next";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 createRoot(document.getElementById("root")).render(
     <>
         <Home />
