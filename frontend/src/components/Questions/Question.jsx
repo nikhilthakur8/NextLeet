1a58b1941c242b1434849b5eb80c55486913897b
Nikhil Thakur <nikhilthakur8012004@gmail.com>
2025-05-29 09:34:10 +0530

    ui changes

--- frontend/src/components/Questions/Question.jsx
+++ frontend/src/components/Questions/Question.jsx
@@ -5,34 +5,37 @@ export const Question = ({ questions, title, footer }) => {
     return (
         questions &&
         questions.length > 0 && (
-            <div className="lg:px-12 px-5 md:py-10 py-5 pb-10" id="latest-question">
+            <div
+                className="lg:px-12 px-5 md:py-10 py-5 pb-10"
+                id="latest-question"
+            >
                 <p className="text-neutral-300 text-center md:mb-10 mb-5 text-xl md:text-2xl xl:text-4xl">
                     <strong className=" bg-clip-text text-transparent bg-gradient-to-t from-gray-400 to-gray-50">
                         {title}
                     </strong>
                 </p>
                 <div className="relative overflow-x-auto shadow-md rounded-md sm:rounded-lg">
-                    <table className="w-full text-sm sm:text-base md:text-md lg:text-lg text-left rtl:text-right text-gray-700 dark:text-gray-400">
-                        <thead className="text-base text-gray-700 uppercase bg-gray-50 dark:bg-neutral-900 dark:text-gray-400">
+                    <table className="w-full text-sm sm:text-base md:text-md  text-left rtl:text-right text-gray-700 dark:text-gray-400">
+                        <thead className="text-gray-700 uppercase bg-gray-50 dark:bg-gray-900 dark:text-gray-400">
                             <tr>
-                                <th scope="col" className="px-6 py-3">
+                                <th scope="col" className="px-6 py-2">
                                     Date
+                                </th>
+                                <th scope="col" className="px-6 py-2">
+                                    Title
                                 </th>
                                 <th
                                     scope="col"
-                                    className="px-6 py-3 whitespace-nowrap"
+                                    className="px-6 py-2 whitespace-nowrap"
                                 >
                                     Question No.
                                 </th>
-                                <th scope="col" className="px-6 py-3">
-                                    Title
-                                </th>
-                                <th scope="col" className="px-6 py-3">
+                                <th scope="col" className="px-6 py-2">
                                     Difficulty
                                 </th>
-                                <th scope="col" className="px-6 py-3">
+                                {/* <th scope="col" className="px-6 py-3">
                                     Link
-                                </th>
+                                </th> */}
                             </tr>
                         </thead>
                         <tbody>
@@ -43,7 +46,7 @@ export const Question = ({ questions, title, footer }) => {
                                         index % 2 === 0
                                             ? "bg-white"
                                             : "bg-gray-50"
-                                    } border-b dark:bg-slate-800 dark:border-gray-700 hover:bg-gray-700 border-gray-200 transition duration-300 ease-in-out cursor-pointer`}
+                                    } border-b dark:bg-slate-800  dark:border-gray-700 hover:bg-gray-700 border-gray-200 transition-all duration-1000 ease-in-out cursor-pointer`}
                                 >
                                     <Row question={question} />
                                 </tr>
@@ -54,7 +57,7 @@ export const Question = ({ questions, title, footer }) => {
                                 <tr>
                                     <td
                                         colSpan="5"
-                                        className="bg-neutral-900 text-white text-center py-2"
+                                        className="bg-gray-900 text-white text-base text-center py-2"
                                     >
                                         {footer}
                                     </td>
--- frontend/src/components/Questions/Row.jsx
+++ frontend/src/components/Questions/Row.jsx
@@ -1,5 +1,7 @@
 import React from "react";
-
+import { Tooltip, TooltipContent, TooltipTrigger } from "../ui/tooltip";
+import { TooltipProvider } from "@radix-ui/react-tooltip";
+import { ExternalLink } from "lucide-react";
 const getDifficultyColor = (difficulty) => {
     if (difficulty === 1) return { text: "Easy", color: "text-green-500" };
     if (difficulty === 2) return { text: "Medium", color: "text-yellow-500" };
@@ -8,19 +10,46 @@ const getDifficultyColor = (difficulty) => {
 export const Row = ({ question }) => {
     return (
         <>
-            <td className="px-6 py-4 text-white whitespace-nowrap">
+            <td className="px-6 py-3 text-white whitespace-nowrap">
                 {new Date(question.date).toDateString()}
             </td>
-            <td className="px-6 py-4 whitespace-nowrap">{question.questionId}</td>
-            <td className="px-6 py-4 font-medium whitespace-nowrap">{question.title}</td>
+            <td className="w-full font-medium whitespace-nowrap text-left text-blue-400 hover:text-blue-500 hover:underline">
+                <a
+                    href={`https://leetcode.com/problems/${question.titleSlug}`}
+                    target="_blank"
+                >
+                    <TooltipProvider delayDuration={100}>
+                        <Tooltip>
+                            <TooltipTrigger asChild>
+                                <div className="cursor-pointer w-full px-6 py-3 text-left flex flex-row items-center space-x-2">
+                                    <span>{question.title}</span>
+                                    <ExternalLink />
+                                </div>
+                            </TooltipTrigger>
+
+                            <TooltipContent
+                                side="top"
+                                align="center"
+                                className="bg-emerald-800 border-none text-sm sm:text-lg text-gray-300"
+                            >
+                                Visit Now
+                            </TooltipContent>
+                        </Tooltip>
+                    </TooltipProvider>
+                </a>
+            </td>
+            <td className="px-6 py-3 whitespace-nowrap">
+                {question.questionId}
+            </td>
+
             <td
-                className={`px-6 py-4 ${
+                className={`px-6 py-3 ${
                     getDifficultyColor(question.difficulty).color
                 }`}
             >
                 {getDifficultyColor(question.difficulty).text}
             </td>
-            <td className="px-6 py-4">
+            {/* <td className="px-6 py-4">
                 <a
                     href={`https://leetcode.com/problems/${question.titleSlug}`}
                     target="_blank"
@@ -28,7 +57,7 @@ export const Row = ({ question }) => {
                 >
                     Link
                 </a>
-            </td>
+            </td> */}
         </>
     );
 };
