7bc6db2551298ee8891858f72be3b59859943a4f
Nikhil Thakur <nikhilthakur8012004@gmail.com>
2025-07-08 03:01:39 +0530

    some fix

--- src/utils/googleLogin.js
+++ src/utils/googleLogin.js
@@ -3,8 +3,8 @@ import { account } from "../appwrite/auth";
 export const googleUserLogin = async () => {
 	account.createOAuth2Token(
 		"google",
-		// "https://nextleet.com/auth/callback",
-		"http://localhost:5173/auth/callback",
+		"https://nextleet.com/auth/callback",
+		// "http://localhost:5173/auth/callback",
 		"https://nextleet.com/login"
 	);
 };
